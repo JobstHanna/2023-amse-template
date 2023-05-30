e7e357667c1131b5edfdd1425aec0aae5c901c22
JobstHanna <hanna-jobst@gmx.de>
2023-05-30 21:14:24 +0200

    project work 4

--- project/auto_testing.py
+++ project/auto_testing.py
@@ -1,41 +1,22 @@
 import pandas as pd
-import sqlite3
-import urllib.request as ur
-from pandas.testing import assert_frame_equal
-from numpy import NaN
+import os.path 
+#import sys
 
-def extract(url):
-    return ur.urlopen(url)
+#sys.path.append( '/data' )
+import data.data_pipeline
 
-def interpreterCsv(data):
-    return pd.read_csv
 
-def tranform(data):
-    return data.dropna()
+def pipeline():
+    data.data_pipeline.gather_data()
 
-def load(data, db):
-    sink = sqlite3.connect(db)
-    data.ro_sql('example', sink, if_exists='replace', index=False)
-    sink.close()
+    if os.path.exists("./data/my_data.sqlite"):
+        print("Test result: No defects were dected sucessful")
+    else:
+        print("Test result: Defects were detected")
     
-def etl(url, db):
-    data = extract(url)
-    data = interpreterCsv(data)
-    data = transfrom(data)
-    load(data, db)
     
-def test_transform():
-    data= pd.DataFrame([[''], [], []])
-    transformed = transfrom(data)
-    
-    assert transformed.shaped =(2, 3)
-    
-def test_load():
-    data = pd.DataFrame([[''], [], []])
-    load(data, 'test.sqlite')
-    
-    sink = sqlite3.connect('test.sqlite')
-    result = pd.read_sql_query("SELECT * FROM example", sink)
-    sink.close()
-    
-    assert_frame_equal(result, data)
+
+
+if __name__ == "__main__":
+    pipeline()
+    #test_cyling_data()
--- project/tests.sh
+++ project/tests.sh
@@ -1,3 +1,6 @@
 #! /bin/bash    
 
 python3 auto_testing.py
+#pytest auto_testing.py
+
+echo "End of testing"
