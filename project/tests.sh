1520b7ead65b5a0f1d97439d0ea3d5025796c299
JobstHanna <hanna-jobst@gmx.de>
2023-06-28 14:40:50 +0200

    final report

--- project/tests.sh
+++ project/tests.sh
@@ -1,11 +1,11 @@
 #! /bin/bash    
 
 echo "start pipeline"
-python3 ./data/data_pipeline.py
+python3 ../data/data_pipeline.py
 
 
 echo "start testing"
-python3 auto_testing.py
+python3 ./auto_testing.py
 #pytest auto_testing.py
 
 echo "end of testing"
